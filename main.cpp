5b5d1858c20471771642025ca9ed24ac1864225f
Frank Kiess <frank_kiess.junior@hotmail.com>
2025-11-07 20:23:53 -0300

    75% -> Estruturando

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <queue>
-#include <algorithm>
-
+#include <string>
+#include <iomanip>
 using namespace std;
 
-struct fila
+struct Paciente
 {
-  int nrSenha;
-  int nrHora;
-  int nrMinuto;
+  string nome;
+  char prioridade;
+  int hora;
+  int minuto;
 };
 
 void menu()
@@ -21,77 +22,124 @@ void menu()
        << "=============================="    << endl;
 }
 
-fila gerarSenha(int &contador)
+int calcularMinutosTotais(int h, int m)
 {
-  filaNova.nrSenha  = ++contador;
+  return h * 60 + m;
 }
 
-void chegadaPaciente(queue<fila> &filaNova, int &contador, const string &tipo)
+void chegadaPaciente(queue<Paciente> &fila, const string &nome, char prior, int hh, int mm)
 {
-  filaNova nova = gerarSenha(contador);
-  filaNova.push(nova);
+  Paciente p = {nome, prior, hh, mm};
+  fila.push(p);
 }
 
-void exibirFilas(queue<fila> filaV, queue<fila> filaA, queue<fila> filaD, queue<fila> filaB)
+void atendimento(queue<Paciente> &fila, int hh, int mm, int &atendidos, int &total, int &tempoMaximo)
 {
+  if (fila.empty()) return;
+
+  int nrTempo  = calcularMinutosTotais(hh, mm);
+  Paciente p = fila.front();
+  fila.pop();
+
+  atendidos++;
+  total++;
+
+  int espera = nrTempo - calcularMinutosTotais(p.hora, p.minuto);
+
+  if (espera > tempoMaximo)
+    tempoMaximo = espera;
+
+  cout << setw(2)         << setfill('0') << hh << ":"
+       << setw(2)         << setfill('0') << mm
+       << " Paciente "    << p.nome
+       << " (Prioridade " << p.prioridade << ") atendido."
+       << endl;
 }
 
-void atendimento(queue<fila> &filaAtendimento, const string &tipo)
-{
-  if (!filaAtendimento.empty())
-  {
-    fila atual = fila.front();
-    filaAtendimento.pop();
-  }
-  else
-    cout << "Nenhum paciente na fila " << tipo << "!\n";
+void exibirFilas(queue<Paciente> filaV, queue<Paciente> filaA, queue<Paciente> filaD, queue<Paciente> filaB, int nrTotal) {
+  cout << "V:" << filaV.size()    << " "
+       << "A:" << filaA.size()    << " "
+       << "D:" << filaD.size()    << " "
+       << "B:" << filaB.size()    << " | "
+       << "Atendidos:" << nrTotal << endl;
 }
 
-void atendimento(queue<fila> &fila, const string &tipo)
-{
-  if (!fila.empty())
-  {
-    fila atual = fila.front();
-    fila.pop();
-  }
-  else
-    cout << "Nenhum paciente na fila " << tipo << "!\n";
+void resumoFinal(int total, int qtFilaV, int qtFilaA, int qtFilaD, int qtFilaB, int pico, int nrTempoMaximo) {
+  cout << "Total atendidos: "       << total         << endl
+       << "V: " << qtFilaV
+       << " A: " << qtFilaA
+       << " D: " << qtFilaD
+       << " B: " << qtFilaB       << endl
+       << "Pico de lotacao: "       << pico          << endl
+       << "Maior tempo de espera: " << nrTempoMaximo << endl;
 }
 
 int main()
 {
-  bool idEncerrado = false;
-  char dsComando;
-  int  nrContadorV = 0,
-       nrContadorA = 0,
-       nrContadorD = 0, 
-       nrContadorB = 0;
+  int qtAtendidosV      = 0,
+      qtAtendidosA      = 0,
+      qtAtendidosD      = 0,
+      qtAtendidosB      = 0,
+      totalAtendidos    = 0,
+      picoLotacao       = 0,
+      tempoMaximoEspera = 0;
+  string comando;
+  queue<Paciente> filaV, filaA, filaD, filaB;
 
-  queue<fila> filaV;
-  queue<fila> filaA;
-  queue<fila> filaD;
-  queue<fila> filaB;
-  fila        aux;
-
-  menu();
-
-  while (!idEncerrado)
+  while (cin >> comando)
   {
-    cin >> dsComando;
-    transform(dsComando.begin(), dsComando.end(), dsComando.begin(), ::toupper);
+    if (comando == "C")
+    {
+      string nome;
+      char   prior;
+      int    hh, mm;
 
-    if (!filaV.empty())
-      atendimento(filaV);
-    else if (filaV.empty() && !filaA.empty())
-      atendimento(filaA);
-    else if (filaA.empty() && !filaD.empty())
-      atendimento(filaD);
-    else
-      atendimento(filaB);
+      cin >> nome >> prior >> hh >> mm; prior = toupper(prior);
+
+      if (prior == 'V')
+        chegadaPaciente(filaV, nome, prior, hh, mm);
+      else if (prior == 'A')
+        chegadaPaciente(filaA, nome, prior, hh, mm);
+      else if (prior == 'D')
+        chegadaPaciente(filaD, nome, prior, hh, mm);
+      else if (prior == 'B')
+        chegadaPaciente(filaB, nome, prior, hh, mm);
+
+      int qtTamanhoFila = filaV.size() + filaA.size() + filaD.size() + filaB.size();
+
+      if (qtTamanhoFila > picoLotacao) 
+        picoLotacao = qtTamanhoFila;
+    }
+
+    else if (comando == "A")
+    {
+      int hh, mm; cin >> hh >> mm;
     
-    if (dsComando = 'Q')
-      idEncerrado = true;
+      if (!filaV.empty())
+        atendimento(filaV, hh, mm, qtAtendidosV, totalAtendidos, tempoMaximoEspera);
+      else if (!filaA.empty())
+        atendimento(filaA, hh, mm, qtAtendidosA, totalAtendidos, tempoMaximoEspera);
+      else if (!filaD.empty())
+        atendimento(filaD, hh, mm, qtAtendidosD, totalAtendidos, tempoMaximoEspera);
+      else if (!filaB.empty())
+        atendimento(filaB, hh, mm, qtAtendidosB, totalAtendidos, tempoMaximoEspera);
+      else
+        cout << setw(2)                                  << setfill('0') << hh << ":"
+             << setw(2)                                  << setfill('0') << mm
+             << " Sem pacientes aguardando atendimento." << endl;
+    }
+
+
+    else if (comando == "D")
+      exibirFilas(filaV, filaA, filaD, filaB, totalAtendidos);
+
+    else if (comando == "Q")
+    {
+      resumoFinal(totalAtendidos, qtAtendidosV, qtAtendidosA, qtAtendidosD, 
+                  qtAtendidosB, picoLotacao, tempoMaximoEspera);
+      break;
+    }
   }
-  
+
   return 0;
 }
