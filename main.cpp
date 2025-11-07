5a7dc1524e37a1e9989dd19d7df91e66703321ff
Frank Kiess <frank_kiess.junior@hotmail.com>
2025-11-06 23:53:43 -0300

    50% Começando a criação da lógica...

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <queue>
-#include "funcoes.cpp"
+#include <algorithm>
 
 using namespace std;
 
@@ -11,9 +11,51 @@ struct fila
   int nrMinuto;
 };
 
-void atendimento(fila &prio)
+void menu()
 {
-  prio.
+  cout << "\n===== MENU DE COMANDOS ====="    << endl
+       << "C -> Chegada de paciente"          << endl
+       << "A -> Atendimento"                  << endl
+       << "D -> Exibir informações das filas" << endl
+       << "Q -> Encerrar a simulação"         << endl
+       << "=============================="    << endl;
+}
+
+fila gerarSenha(int &contador)
+{
+  filaNova.nrSenha  = ++contador;
+}
+
+void chegadaPaciente(queue<fila> &filaNova, int &contador, const string &tipo)
+{
+  filaNova nova = gerarSenha(contador);
+  filaNova.push(nova);
+}
+
+void exibirFilas(queue<fila> filaV, queue<fila> filaA, queue<fila> filaD, queue<fila> filaB)
+{
+}
+
+void atendimento(queue<fila> &filaAtendimento, const string &tipo)
+{
+  if (!filaAtendimento.empty())
+  {
+    fila atual = fila.front();
+    filaAtendimento.pop();
+  }
+  else
+    cout << "Nenhum paciente na fila " << tipo << "!\n";
+}
+
+void atendimento(queue<fila> &fila, const string &tipo)
+{
+  if (!fila.empty())
+  {
+    fila atual = fila.front();
+    fila.pop();
+  }
+  else
+    cout << "Nenhum paciente na fila " << tipo << "!\n";
 }
 
 int main()
@@ -24,16 +66,19 @@ int main()
        nrContadorA = 0,
        nrContadorD = 0, 
        nrContadorB = 0;
+
   queue<fila> filaV;
   queue<fila> filaA;
   queue<fila> filaD;
   queue<fila> filaB;
+  fila        aux;
 
   menu();
 
   while (!idEncerrado)
   {
     cin >> dsComando;
+    transform(dsComando.begin(), dsComando.end(), dsComando.begin(), ::toupper);
 
     if (!filaV.empty())
       atendimento(filaV);
@@ -44,9 +89,6 @@ int main()
     else
       atendimento(filaB);
     
-
-      
-
     if (dsComando = 'Q')
       idEncerrado = true;
   }
